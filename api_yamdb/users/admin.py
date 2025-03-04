0e0fab87c926d7cb1f149d47ddb9e5d8c1e79b54
mishatunikov <michael.tunikov@gmail.com>
2025-03-04 20:00:09 +0300

    Добавленые новые поля в стандартное отображение модели пользователя в админке.

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+
+UserAdmin.fieldsets += (
+    ('Extra fields', {'fields': ('role', 'bio', )}),
+)
+
+admin.site.register(CustomUser, UserAdmin)

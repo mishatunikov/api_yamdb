11e86a74500e9909f9e7ae1988e7ba50b2deb6af
Kostya Golovko <golowko.costya2010@gmail.com>
2025-03-07 15:47:14 +0300

    add review and comment models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -86,3 +87,67 @@ class Title(models.Model):
 
     def __str__(self):
         return f'{self.name}, {str(self.year)}, {self.category}'
+
+
+class ReviewCommentModel(models.Model):
+    """Абстрактная модель для отзывов и комментариев."""
+    text = models.TextField('Текст')
+    author = models.ForeignKey(
+        User,
+        verbose_name="Автор",
+        on_delete=models.CASCADE,
+    )
+    pub_date = models.DateTimeField(
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True,
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text
+
+
+class Review(ReviewCommentModel):
+    """Модель для отзывов пользователей на произведения."""
+    title = models.ForeignKey(
+        Title, verbose_name='Произведение', on_delete=models.CASCADE
+    )
+    score = models.SmallIntegerField(
+        'Оценка произведения',
+        validators=[
+            MinValueValidator(
+                1, message='Оценка должна быть больше или равна 1'
+            ),
+            MaxValueValidator(
+                10, message='Оценка должна быть меньше или равна 10'
+            ),
+        ],
+        default=1,
+    )
+
+    class Meta(ReviewCommentModel.Meta):
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        default_related_name = 'reviews'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('title', 'author'), name='unique_review'
+            ),
+        ]
+
+
+class Comment(ReviewCommentModel):
+    """Модель комментариев к отзывам."""
+    review = models.ForeignKey(
+        Review, verbose_name='Отзыв', on_delete=models.CASCADE,
+        related_name='comments'
+    )
+
+    class Meta(ReviewCommentModel.Meta):
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        default_related_name = 'comments'

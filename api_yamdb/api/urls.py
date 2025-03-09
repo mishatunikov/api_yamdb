a3bd1ffc58d40940cac620d906221c7ee6123cdc
mishatunikov <michael.tunikov@gmail.com>
2025-03-09 13:49:23 +0300

    Добавлены эндпоинты к users/.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,27 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
-from api.views import SignUpAPIView, TokenAccessObtainView
+from api.views import SignUpAPIView, TokenAccessObtainView, UserViewSet
+
+router = DefaultRouter()
+
+router.register('users', UserViewSet)
+
 
 auth_endpoints = [
-    path('signup/', SignUpAPIView.as_view()),
-    path('token/', TokenAccessObtainView.as_view())
+    path('signup/', SignUpAPIView.as_view(), name='signup'),
+    path('token/', TokenAccessObtainView.as_view(), name='access_token'),
 ]
 
 
 urlpatterns = [
-    path('v1/auth/', include(auth_endpoints))
+    path('v1/auth/', include(auth_endpoints)),
+    path(
+        'v1/users/me/',
+        UserViewSet.as_view(
+            {'get': 'current_user_data', 'patch': 'current_user_data'}
+        ),
+        name='users_me',
+    ),
+    path('v1/', include(router.urls)),
 ]

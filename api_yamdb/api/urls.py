d2e605998a054607479f0f27881e0593f4b207cb
Kostya Golovko <golowko.costya2010@gmail.com>
2025-03-11 15:30:59 +0300

    url для комментариев и отзывов

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -8,6 +8,8 @@ from api.views import (
     CategoryViewSet,
     TitleViewSet,
     GenreViewSet,
+    CommentViewSet,
+    ReviewViewSet,
 )
 
 router_v1 = SimpleRouter()
@@ -16,6 +18,15 @@ router_v1.register(r'users', UserViewSet)
 router_v1.register(r'titles', TitleViewSet, basename='title')
 router_v1.register(r'genres', GenreViewSet, basename='genre')
 router_v1.register(r'categories', CategoryViewSet, basename='category')
+router_v1.register('users', UserViewSet)
+router_v1.register(
+    r"titles/(?P<title_id>\d+)/reviews", ReviewViewSet, basename="review"
+)
+router_v1.register(
+    r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
+    CommentViewSet,
+    basename="comment",
+)
 
 
 auth_endpoints = [

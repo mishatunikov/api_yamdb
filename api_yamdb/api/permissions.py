107885ccbf110b5d9d81769c2bc8cae824562311
mishatunikov <michael.tunikov@gmail.com>
2025-03-12 12:03:22 +0300

    Переписаны пермишины с использованием новых свойств модели.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -8,9 +8,7 @@ class IsAdminOrSuperuser(BasePermission):
     """
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated and (
-            request.user.is_superuser or request.user.role == 'admin'
-        )
+        return request.user.is_authenticated and request.user.is_admin
 
 
 class IsAdminOrReadOnly(IsAdminOrSuperuser):
@@ -38,7 +36,6 @@ class IsAdminOrOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in SAFE_METHODS
-            or request.user.role == 'admin'
-            or request.user.role == 'moderator'
+            or request.user.is_admin_or_moderator
             or obj.author == request.user
         )

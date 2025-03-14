52684c3b6bbd137a4ec8794cae933b879a66c449
mishatunikov <michael.tunikov@gmail.com>
2025-03-14 12:29:37 +0300

    Убрана поддержка мультиязычности в поле email.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
     """Кастомная модель пользователя."""
 
-    email = models.EmailField(_("email address"), unique=True)
+    email = models.EmailField(unique=True)
     role = models.CharField(
         choices=[
             ('user', 'пользователь'),
